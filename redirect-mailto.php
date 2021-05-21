9fb01dbeff7710f39cc77c022c97de959ab6f82a
FlorianClemente <84383326+FlorianClemente@users.noreply.github.com>
2021-05-21 15:59:31 +0200

    Update redirect-mailto.php

--- redirect-mailto.php
+++ redirect-mailto.php
@@ -1,3 +1,3 @@
 <?php
-header("Location: mailto:don_clemente@web.de");
+header("Location: mailto:&#x66;&#x6c;&#x6f;&#x72;&#x69;&#x61;&#x6e;&#x2e;&#x63;&#x6c;&#x65;&#x6d;&#x65;&#x6e;&#x74;&#x65;&#x40;&#x67;&#x6d;&#x61;&#x69;&#x6c;&#x2e;&#x63;&#x6f;&#x6d;");
 ?>
